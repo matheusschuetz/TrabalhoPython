a13d230843e998ba3d2c7847ceec97eecc9b3e85
Matheus <matheusschuetz0213@gmail.com>
2020-01-13 11:55:10 -0300

    Aula32

---
+++ Aula32/aula32.py
@@ -0,0 +1,26 @@
+#flask_mysqldb
+import MySQLdb
+
+
+def listar_todos(c):
+    c.execute('SELECT * FROM pessoas')
+    pessoas = c.fetchall()
+    for p in pessoas:
+        print(p)
+
+def buscar_por_id(c,id):
+    c.execute(f'SELECT * FROM pessoa WHERE ID = {id}')
+    pessoa = c.fetchone()
+    print(pessoa)
+
+def buscar_por_sobrenome(c,sobrenome):
+    c.execute(f'SELECT * FROM pessoa WHERE Sobrenome like {sobrenome}%')
+    pessoa = c.fetchone()
+    for p in pessoas:
+        print(p)
+
+conexao = MySQLdb.connect(host= '127.0.0.1', database='aula1', user='root')
+cursor=conexao.cursor()
+# listar_todos(cursor)
+# buscar_por_id(cursor, 8)
+# buscar_por_sobrenome(cursor, 'Sc')
